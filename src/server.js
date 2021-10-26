6f289a93d168df923306a355d2e449b7469d4b56
HDTran91 <KhoaiGa0103@gmail.com>
2021-10-25 21:07:11 -0500

    Routes_and_Controller

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./config/connectDB";
 import configViewEngine from "./config/viewEngine";
+import initRoutes from "./routes/web";
 
 
 //Init app
@@ -13,12 +14,8 @@ connectDB();
 
 configViewEngine(app);
 
-app.get("/", (req,res)=> {
-  return res.render("main/master");
-});
-app.get("/login-register", (req,res)=> {
-  return res.render("auth/loginRegister");
-});
+//init routes
+initRoutes(app);
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, ()=> {
   console.log(`Hello Hoang Tran,I am running at ${process.env.APP_HOST}:${process.env.APP_PORT}/` )

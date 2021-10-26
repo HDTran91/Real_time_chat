724e4b9305f5952a660020569f1670698ea6af85
HDTran91 <KhoaiGa0103@gmail.com>
2021-10-25 19:31:16 -0500

    add_static_pages

---
+++ src/config/viewEngine.js
@@ -0,0 +1,16 @@
+import { config } from "bluebird";
+import express from "express";
+import expressEjsExtend from "express-ejs-extend";
+
+/**
+ * config view engine for app
+ */
+
+let configViewEngine = (app) => {
+  app.use (express.static("./src/public"));
+  app.engine("ejs", expressEjsExtend);
+  app.set("view engine", "ejs");
+  app.set("views","./src/views");
+}
+
+module.exports = configViewEngine;
--- src/server.js
+++ src/server.js
@@ -1,24 +1,23 @@
 import express from "express";
 import connectDB from "./config/connectDB";
-import ContactModel from "./models/contact.model";
+import configViewEngine from "./config/viewEngine";
 
+
+//Init app
 let app = express();
 
 //connect to MongoDB
 connectDB();
 
+//config ViewEngine
 
-app.get("/test-database", async (req,res)=> {
-  try {
-    let item ={
-      userId: "@!#!@$",
-      contactId: "124124124"
-    };
-    let contact = await ContactModel.createNew(item);
-    res.send(contact);
-  }catch(err){
-    console.log (err);
-  }
+configViewEngine(app);
+
+app.get("/", (req,res)=> {
+  return res.render("main/master");
+});
+app.get("/login-register", (req,res)=> {
+  return res.render("auth/loginRegister");
 });
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, ()=> {
